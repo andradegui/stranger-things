4b23d3dc75c785ccf869522ebb9a478626818eed
Guilherme Lima <guilima102013@gmail.com>
2026-02-28 14:31:01 -0300

    Aula 04 Stranger Things wip: Ajuste responsividade c/ animações GSAP feat: Animações de paralax, scroll e exibição de textos c/ GSAP

--- script.js
+++ script.js
@@ -26,14 +26,49 @@ gsap.from("picture:nth-child(2)", {
 
 gsap.from(".card", {
     opacity: 0,
-    duration: 1,
-    y: 30,
-    filter: "blur(20px)",
+    // duration: 1,
+    y: 5,
+    filter: "blur(5px)",
+    // Uma animação é animada atrás da outra (intercalada)
     stagger: .3,
     scrollTrigger: {
         trigger: ".cards",
-        markers: true
+        // markers: true,
+        start: "0% 80%",
+        end: "100% 70%",
+        scrub: true
     }
-})
+});
+
+gsap.from(".secaoObrigado ul li", {
+    opacity: 0,
+    x: 40,
+    filter: "blur(40px)",
+    stagger: .1,
+    scrollTrigger: {
+        trigger: ".secaoObrigado ul",
+        // markers: true,
+        start: "0% 80%",
+        end: "100% 50%",
+        scrub: true
+    }
+
+});
+
+gsap.from("footer", {
+    y: "-30%",
+    // Renderiza a página sem jogar o posicionamento acima
+    immediateRender: false,
+    scrollTrigger: {
+        trigger: "footer",
+        // Para ficar sincronizado com a rolagem da página
+        scrub: true,
+        // markers: true,
+        invalidateOnRefresh: true,
+        end: "100% 100%"
+    }
+});
+
+
 
 
